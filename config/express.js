0521ca1f6e97d1ed5b84cc9d60c3e42c90e8af12
Daniel Vieira <daniellimafv@yahoo.com.br>
2021-05-25 12:48:49 +0100

    add cors

--- config/express.js
+++ config/express.js
@@ -1,10 +1,13 @@
 const express    = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const config     = require('config');
 const consign    = require('consign');
 
 module.exports = () => {
   const app = express();
+
+  app.use(cors())
 
   // SETANDO VARIÁVEIS DA APLICAÇÃO
   app.set('port', process.env.PORT || config.get('server.port'));
